2917248fb564b8ae4b3ae16411601cb1775f066d
Matthew Denune <mattdenune@gmail.com>
2024-05-07 14:34:02 -0400

    converted Bootstrap to css in InfoSection and Carousel components

---
+++ src/components/InfoSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/infoSection.css";
+
+const InfoSection = () => (
+  <section className="page-section" id="services">
+    <div class="container-fluid">
+      <div class="container">
+        <h1 class="containerText">Underwater Inspection Services</h1>
+        <hr />
+        <h4>
+          VISION SUBSEA utilizes Remotely Operated Vehicles and multibeam sonar
+          equipment to provide high-quality cost-effective data collection for
+          clients across Alaska.
+        </h4>
+      </div>
+    </div>
+  </section>
+);
+
+export default InfoSection;
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -4,6 +4,8 @@ import landingVideo from "../media/landingVideo_Compressed.mp4";
 import logo from '../media/Logo_VS Horizontal white.png';
 import customerComments from '../Data/customer_comments';
 import Hero from "./Hero";
+import InfoSection from "./InfoSection";
+import Carousel from "./Carousel";
 
 
 const Landingpage = () => (
@@ -14,64 +16,48 @@ const Landingpage = () => (
   </>
 )
 
-const InfoSection = () => (
-  <section className="page-section mt-5" id="services">
-    <div class="container-fluid text-center p-5">
-      <div class="container p-5">
-        <h1 class="display-4">Underwater Inspection Services</h1>
-        <hr />
-        <h4>
-          VISION SUBSEA utilizes Remotely Operated Vehicles and multibeam sonar
-          equipment to provide high-quality cost-effective data collection for
-          clients across Alaska.
-        </h4>
-      </div>
-    </div>
-  </section>
-);
-
-const Carousel = () => (
-  <section className="carouselSection bg-light d-flex justify-content-center align-items-start">
-    <div
-      id="carouselExampleControls"
-      className="innerContainer carousel slide"
-      data-bs-ride="carousel"
-    >
-      <div className="carousel-inner">
-        {customerComments.map((comment, i) => (
-          <div
-            className={`carousel-item ${i === 0 ? "active" : ""}`}
-            key={i}
-          >
-            <figure className="text-center">
-              <blockquote className="blockquote">
-                <p className="h3">"{comment.comment}"</p>
-              </blockquote>
-              <p className="h4 text-muted">- {comment.name}</p>
-            </figure>
-          </div>
-        ))}
-      </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
-    </div>
-  </section>
-);
+// const Carousel = () => (
+//   <section className="carouselSection bg-light d-flex justify-content-center align-items-start">
+//     <div
+//       id="carouselExampleControls"
+//       className="innerContainer carousel slide"
+//       data-bs-ride="carousel"
+//     >
+//       <div className="carousel-inner">
+//         {customerComments.map((comment, i) => (
+//           <div
+//             className={`carousel-item ${i === 0 ? "active" : ""}`}
+//             key={i}
+//           >
+//             <figure className="text-center">
+//               <blockquote className="blockquote">
+//                 <p className="h3">"{comment.comment}"</p>
+//               </blockquote>
+//               <p className="h4 text-muted">- {comment.name}</p>
+//             </figure>
+//           </div>
+//         ))}
+//       </div>
+//       <button
+//         className="carousel-control-prev"
+//         type="button"
+//         data-bs-target="#carouselExampleControls"
+//         data-bs-slide="prev"
+//       >
+//         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+//         <span className="visually-hidden">Previous</span>
+//       </button>
+//       <button
+//         className="carousel-control-next"
+//         type="button"
+//         data-bs-target="#carouselExampleControls"
+//         data-bs-slide="next"
+//       >
+//         <span className="carousel-control-next-icon" aria-hidden="true"></span>
+//         <span className="visually-hidden">Next</span>
+//       </button>
+//     </div>
+//   </section>
+// );
 
 export default Landingpage;
