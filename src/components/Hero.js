15b5be3dbd500aaf8138cc44a760205df2c64e75
Matthew Denune <mattdenune@gmail.com>
2024-05-07 14:01:48 -0400

    converted Bootstrap to css in Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = ({ video, logo, text }) => (
 
     <div className="home-text">
       {logo && (
-        <img src={logo} alt="Vision Subsea logo" style={{ width: "50vw" }} />
+        <img src={logo} alt="Vision Subsea logo" className="heroLogo" />
       )}
       <p>{text}</p>
     </div>
