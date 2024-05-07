17d9985b7afee2849c3d01efc4174ed9868daddd
Matthew Denune <mattdenune@gmail.com>
2024-05-07 13:59:05 -0400

    converted Bootstrap to css in footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from '../media/Logo_VS Horizontal Color.png';
 
+import '../styles/footer.css'
+
 
 const Footer = () => (
-  <div className='text-center my-2'>
-    <img src={logo} alt='vision subsea logo' style={{width: "200px"}} />
+  <div className='footerContainer'>
+    <img src={logo} alt='vision subsea logo' className='footerLogo'/>
     <p>Alaska, USA</p>
     <p>907-299-4403</p>
     <p>info@visionsubsea.com</p>
